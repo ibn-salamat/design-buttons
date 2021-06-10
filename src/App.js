d79691312134f08206fa3a722ddff035dff1c4c5
Nurlykhan <nurlykhan.salamatuly@samgau.com>
2021-06-10 13:11:37 +0600

    init: antd feat: add styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Button } from "antd";
+import { DownloadOutlined, PlusCircleOutlined } from "@ant-design/icons";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Button>default</Button>
+      <Button className="btn-default ">default</Button>
+      <Button disabled className="btn-default">
+        default disabled
+      </Button>
+      <br />
+      <Button className="btn-default primary">Primary</Button>
+      <Button disabled className="btn-default primary">
+        Primary disabled
+      </Button>
+      <br />
+      <Button className="btn-default warning">Warning</Button>{" "}
+      <Button disabled className="btn-default warning">
+        Warning disabled
+      </Button>
+      <br />
+      <Button className="btn-default error">Error</Button>
+      <Button disabled className="btn-default error">
+        Error disabled
+      </Button>
+      <br />
+      <Button className="btn-default success">Success</Button>{" "}
+      <Button disabled className="btn-default success">
+        Success disabled
+      </Button>
+      <hr style={{ margin: "15px 0" }} />
+      <h1>Text + Icon</h1>
+      <Button className="btn-default ">
+        <PlusCircleOutlined />
+        default
+      </Button>
+      <Button disabled className="btn-default">
+        <PlusCircleOutlined />
+        default disabled
+      </Button>
+      <br />
+      <Button className="btn-default primary">
+        <PlusCircleOutlined />
+        Primary
+      </Button>
+      <Button disabled className="btn-default primary">
+        <PlusCircleOutlined />
+        Primary disabled
+      </Button>
+      <br />
+      <Button className="btn-default warning">
+        <PlusCircleOutlined />
+        Warning
+      </Button>
+      <Button disabled className="btn-default warning">
+        <PlusCircleOutlined />
+        Warning disabled
+      </Button>
+      <br />
+      <Button className="btn-default error">
+        <PlusCircleOutlined />
+        Error
+      </Button>
+      <Button disabled className="btn-default error">
+        <PlusCircleOutlined />
+        Error disabled
+      </Button>
+      <br />
+      <Button className="btn-default success">
+        <PlusCircleOutlined />
+        Success
+      </Button>
+      <Button disabled className="btn-default success">
+        <PlusCircleOutlined />
+        Success disabled
+      </Button>
+      <hr style={{ margin: "15px 0" }} />
+      <h1>Icon</h1>
+      <Button className="btn-icon">
+        <DownloadOutlined />
+      </Button>
+      <Button disabled className="btn-icon">
+        <DownloadOutlined />
+      </Button>
+      <br />
+      <Button className="btn-icon warning">
+        <DownloadOutlined />
+      </Button>
+      <Button disabled className="btn-icon warning">
+        <DownloadOutlined />
+      </Button>
+      <hr style={{ margin: "15px 0" }} />
+      <h1>Link</h1>
+      <Button className="btn-link warning">
+        <DownloadOutlined />
+        Download
+      </Button>
+      <Button disabled className="btn-link warning">
+        <DownloadOutlined />
+        Download
+      </Button>
+    </>
   );
 }
 
